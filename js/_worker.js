3623e7eb33279878644026b2350f1281e6010adf
kcepu877 <kcepu877@gmail.com>
2025-03-28 19:50:38 +0700

    Update _worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -1,6 +1,6 @@
 import { connect } from "cloudflare:sockets";
 
-const proxyListURL = 'https://bmkg.xyzupdate_proxyip.txt';
+const proxyListURL = 'https://bmkg.xyz/update_proxyip.txt';
 const pagehost = '/'
 const namaWeb = 'FREE PROXY LIFETIME'
 const telegramku = 'https://t.me/seaker877'
