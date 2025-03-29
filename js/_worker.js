0efd61822bf09d74d199ab3259b0c859820e63bc
kcepu877 <kcepu877@gmail.com>
2025-03-30 03:10:45 +0700

    Update _worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -1383,7 +1383,7 @@ async function handleSubRequest(hostnem) {
                 <div class="form-group">
                     <label for="bug">Bug</label>
                     <select id="bug" class="form-control" required>
-                    <option value="">NO BUG</option>
+                    <option value="MASUKAN-BUG-COM">NO BUG</option>
                     <option value="business.blibli.com">business.blibli.com</option>
                     <option value="ava.game.naver.com">ava.game.naver.com</option>
                     <option value="graph.instagram.com">graph.instagram.com</option>
